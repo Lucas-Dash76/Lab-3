57cca064b01d52b1eb15857fdfa1db177f90e71f
2389873 <2389873@host.docker.internal>
2025-09-17 09:42:44 -0400

    first commit

---
+++ src/lab_3_lucas_dash/Lab_3_Lucas_Dash.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_3_lucas_dash;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2389873
+ */
+public class Lab_3_Lucas_Dash extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label messageLabel = new Label("Name: ");
+        HBox hb = new HBox(messageLabel);
+        Scene s = new Scene(hb, 1000, 500);
+        stage.setTitle("First application");
+        stage.setScene(s);
+        stage.show();
+    }
+    
+}
